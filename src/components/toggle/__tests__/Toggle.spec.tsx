78af3de5d28db2de57b45a315a800e8a76f6a061
aaho <adam.aho@arcticwolf.com>
2020-06-29 15:48:22 -0400

    adding toggle component without tests

---
+++ src/components/toggle/__tests__/Toggle.spec.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+import { shallow } from 'enzyme';
+
+import { Toggle } from '../Toggle';
+
+describe('Toggle', () => {
+  it('renders', () => {
+    // your test case here
+  });
+});
