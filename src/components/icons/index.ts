c8a5e9c89885de4cdeada8375831810109f468af
aaho <adam.aho@arcticwolf.com>
2020-07-27 15:23:46 -0400

    adding file drag and drop component

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -11,11 +11,13 @@ import Exclamation from './Exclamation';
 import ExclamationCircle from './ExclamationCircle';
 import ExclamationTriangle from './ExclamationTriangle';
 import EyeSlash from './EyeSlash';
+import File from './File';
 import Folder from './Folder';
 import FolderOpen from './FolderOpen';
 import InfoCircle from './InfoCircle';
 import Loading from './Loading';
 import Minus from './Minus';
+import PaperClip from './PaperClip';
 import Pencil from './Pencil';
 import Plus from './Plus';
 import Search from './Search';
@@ -24,6 +26,7 @@ import StickyNote from './StickyNote';
 import TicketAlt from './TicketAlt';
 import Times from './Times';
 import TimesCircle from './TimesCircle';
+import TrashAlt from './TrashAlt';
 import WindowClose from './WindowClose';
 
 export const Icon = {
@@ -40,11 +43,13 @@ export const Icon = {
   ExclamationCircle,
   ExclamationTriangle,
   EyeSlash,
+  File,
   Folder,
   FolderOpen,
   InfoCircle,
   Loading,
   Minus,
+  PaperClip,
   Pencil,
   Plus,
   Search,
@@ -53,6 +58,7 @@ export const Icon = {
   TicketAlt,
   Times,
   TimesCircle,
+  TrashAlt,
   WindowClose,
 };
 
